cb668c15f63f2b66509dc5c8461b7ae06c60a80e
Geenoos <freiya1002@naver.com>
2021-06-13 06:32:20 +0900

    메모 데이터 배열에 json파일을 담을 수 있도록 변경

--- memoproject/src/components/MemoProject.js
+++ memoproject/src/components/MemoProject.js
@@ -8,10 +8,14 @@ import Footer from "./Footer";
 
 function MemoProject() {
     const [data, setData] = useState({
-        memolist: [
-            {id: uuid(), title: "제목1", content: "내용1", date: "날짜1"},
-            {id: uuid(), title: "제목2", content: "내용2", date: "날짜2"},
-            {id: uuid(), title: "제목3", content: "내용3", date: "날짜3"}
+        memolist: [{
+            id: uuid(), 
+            title: "메모를 작성해보세요", 
+            content: "클릭", 
+            date: "2021-01-01",
+            title_json: null,
+            content_json: null,
+            },
         ],
     })
 
@@ -26,10 +30,10 @@ function MemoProject() {
     }
     const handleSave = (id, update) => {
         if(data.memolist.some(memo => memo.id == id)){
-            setData({memolist: data.memolist.map(memo => memo.id == id ? {id: memo.id, title: update.title, content: update.content, date: update.date} : memo)});
+            setData({memolist: data.memolist.map(memo => memo.id == id ? {id: memo.id, title: update.title, content: update.content, date: update.date, title_json:update.title_json, content_json:update.title_json} : memo)});
         }
         else{
-            setData({memolist: [{id: id, title: update.title, content: update.content, date: update.date}].concat(data.memolist)});
+            setData({memolist: [{id: id, title: update.title, content: update.content, date: update.date, title_json:update.title_json, content_json:update.title_json}].concat(data.memolist)});
         }
     }
 
